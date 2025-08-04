abf93f51ea00c874ff6f3c2010f557f92619f447
Luigi <71022962+gifuniluigi@users.noreply.github.com>
2025-08-04 02:16:47 +0200

    Add files via upload

---
+++ appendix/tables/TableF7/5-BVAR/Print_Table_F7_5BVAR.m
@@ -0,0 +1,23 @@
+%%% This file produces the results reported in Table F7, 4-BVAR
+
+clear; clc;
+
+% Add path to functions
+addpath('functions');
+
+% Load results: 5BVAR1 refers to results obtained using Scount.
+%               5BVAR2 refers to results obtained using Dictionary: Financial Stability (Correa et al.[2017])
+%               and so on...see "Get_Table" file for the name of all text series
+load 5BVAR1
+
+% compute and display MSPEs
+T=print_MSPE(msfe);
+
+% compute and display Diebold-Mariano test
+print_dmpval(msfe,nmodel,nfore)
+
+% latex tabel
+printLatex(T)
+
+%close path
+rmpath('functions');
