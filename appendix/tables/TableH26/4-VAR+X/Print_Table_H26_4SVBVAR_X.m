e2595fc38994c687a176f8bf0cefe44c540830f8
Luigi <71022962+gifuniluigi@users.noreply.github.com>
2025-08-04 02:36:47 +0200

    Add files via upload

---
+++ appendix/tables/TableH26/4-VAR+X/Print_Table_H26_4SVBVAR_X.m
@@ -0,0 +1,23 @@
+%%% This file produces the results reported in Table H26, 4-SVBVARX
+
+clear; clc;
+
+% Add path to functions
+addpath('functions');
+
+% Load results: 4SVBVAR_X1 refers to results obtained using Ucount.
+%               4SVBVAR_X2 refers to results obtained using Boolean
+%               and so on...see "Get_Table" file for the name of all text series
+load 4SVBVAR_X1
+
+% compute and display MSPEs
+T=print_MSPE(msfe);
+
+% compute and display Diebold-Mariano test
+print_dmpval(msfe,nmodel,nfore)
+
+% latex tabel
+printLatex(T);
+
+%close path
+rmpath('functions');
