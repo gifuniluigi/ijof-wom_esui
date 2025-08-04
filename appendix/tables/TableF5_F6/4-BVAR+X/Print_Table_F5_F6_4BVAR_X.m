97ad791b9e67ffd5ce3c2be0a11473711df501d6
Luigi <71022962+gifuniluigi@users.noreply.github.com>
2025-08-04 02:13:19 +0200

    Add files via upload

---
+++ appendix/tables/TableF5_F6/4-BVAR+X/Print_Table_F5_F6_4BVAR_X.m
@@ -0,0 +1,23 @@
+%%% This file produces the results reported in Tables F5 & F6, 4-BVARX
+
+clear; clc;
+
+% Add path of functions
+addpath('functions');
+
+% Load results: 4BVAR_X1 refers to results obtained using Scount.
+%               4BVAR_X2 refers to results obtained using Dictionary: Financial Stability (Correa et al.[2017])
+%               and so on...see "Get_Table" file for the name of all text series
+load 4BVAR_X1
+
+% compute and display MSPEs
+T=print_MSPE(msfe);
+
+% compute and display Diebold-Mariano test
+print_dmpval(msfe,nmodel,nfore)
+
+% latex tabel
+printLatex(T);
+
+%close path
+rmpath('functions');
---
+++ appendix/tables/TableF5_F6/4-BVAR/functions/printLatex.m
@@ -0,0 +1,7 @@
+function printLatex(T)
+
+Tarr=table2array(T);
+WTI=cat(2,num2str(Tarr(1,1)),' \\ ',num2str(Tarr(1,2)),' \\ ',num2str(Tarr(1,3)),' \\ ',num2str(Tarr(1,4)),' \\ ',num2str(Tarr(1,5)))
+RAC=cat(2,num2str(Tarr(2,1)),' \\ ',num2str(Tarr(2,2)),' \\ ',num2str(Tarr(2,3)),' \\ ',num2str(Tarr(2,4)),' \\ ',num2str(Tarr(2,5)))
+BRENT=cat(2,num2str(Tarr(3,1)),' \\ ',num2str(Tarr(3,2)),' \\ ',num2str(Tarr(3,3)),' \\ ',num2str(Tarr(3,4)),' \\ ',num2str(Tarr(3,5)))
+end
